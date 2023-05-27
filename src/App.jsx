0b4d570f575ffd001d49639150760d571af5cc63
JayashTripathy <jayashtripathy371@gmail.com>
2023-05-28 00:23:35 +0530

    made left side container

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/navbar'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Home from "./components/homepage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
-
       <Navbar />
-
+      
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/homepage.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "../styles/homepage.css";
+
+function homepage() {
+  return (
+    <div className="homepage">
+      <div className="content">
+        <div className="head-text">
+          Access curated courses worth
+          <div className="sub-head">
+            ₹ <span className="red-cut">18,500</span> at just{" "}
+            <span className="highlight grow">₹ 99 </span>per year!
+          </div>
+        </div>
+        <div className="features">
+          <div className="feature">
+            <img src="src\assets\book.png" width="45px" alt="" />
+            <div>
+              <span className="highlight">100+</span> Job relevant courses
+            </div>
+          </div>
+
+          <div className="feature">
+            <img src="src\assets\clock.png" width="45px" alt="" />
+            <div>
+              <span className="highlight">20,000+</span> Hours of contents
+            </div>
+          </div>
+          <div className="feature">
+            <img src="src\assets\live.png" width="45px" alt="" />
+            <div>
+              <span className="highlight">Exclusive</span> webinar access
+            </div>
+          </div>
+          <div className="feature">
+            <img src="src\assets\scholarship.png" width="45px" alt="" />
+            <div>
+              Scholarship worth
+              <span className="highlight"> ₹94,500</span>
+            </div>
+          </div>
+          <div className="feature">
+            <img src="src\assets\ads.png" width="45px" alt="" />
+            <div>
+              <span className="highlight">Ad Free</span> learning experience
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default homepage;
