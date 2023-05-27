5f1828e52ff8f0d33552eba81b3644dc9b538f9a
JayashTripathy <jayashtripathy371@gmail.com>
2023-05-27 17:25:54 +0530

    done navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./navbar.css";
+import { FaChevronDown } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
+
+function navbar() {
+  return (
+    <header id="Navbar" className="navbar">
+      <div className="nav-container">
+        <div className="primary-options">
+          <div className="main-logo">EDYODA</div>
+
+          <div className="re-pages">
+            <div>
+              Courses
+              <FaChevronDown />
+            </div>
+            <div>
+              Programs <FaChevronDown />
+            </div>
+          </div>
+        </div>
+
+        <div className="right-options">
+          <AiOutlineSearch />
+          <div>Log in</div>
+          <button className="join-btn">
+            Join Now
+        </button>
+        </div>
+        
+       
+      </div>
+    </header>
+  );
+}
+
+export default navbar;
